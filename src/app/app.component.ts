028160eaeb2fb53e3d62d115234b2dac58a0b16b
fateru victor <fateruvictor96@gmail.com>
2022-10-11 02:59:26 +0100

    filter by credit and debit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,29 +12,32 @@ export class AppComponent implements OnInit {
   selectedBank?: number;
   selectedOption?: boolean;
   transactionType = [
-    { id: 1, name: 'pos' },
-    { id: 2, name: 'gtb' },
-    { id: 3, name: 'interbank' },
-    { id: 4, name: 'Nibss' },
+    { id: 1, name: 'all', debit: true },
+    { id: 2, name: 'pos', debit: true },
+    { id: 3, name: 'gtb', debit: true },
+    { id: 4, name: 'interbank', debit: true },
+    { id: 5, name: 'Nibss', debit: true },
+    { id: 6, name: 'inflow', debit: true },
+    { id: 7, name: 'outflow', debit: true },
+  ];
 
-  ]
+  inflowArray: any = [];
+
+
 
   transactionHistory = [
-    { id: 1, narration: 'pos transaction' },
-    { id: 2, narration: 'gtb transaction' },
-    { id: 3, narration: 'interbank transaction'},
-    { id: 4, narration: 'Nibss transaction' },
-    { id: 5, narration: 'gtb transaction' },
-    { id: 6, narration: 'interbank transaction' },
-    { id: 7, narration: 'Nibss transaction' },
-    { id: 8, narration: 'pos transaction' },
-    
-    { id: 9, narration: 'gtb transaction' },
-    { id: 10, narration: 'pos transaction' },
-    
-    { id: 11, narration: 'gtb transaction' },
-    { id: 12, narration: 'Nibss transaction' },
-    
+    { id: 1, narration: 'pos transaction', amountCredit: "", amountDebit: "100" },
+    { id: 2, narration: 'gtb transaction', amountCredit: "", amountDebit: "100" },
+    { id: 3, narration: 'interbank transaction', amountCredit: "", amountDebit: "100" },
+    { id: 4, narration: 'Nibss transaction', amountCredit: "200", amountDebit: "" },
+    { id: 5, narration: 'gtb transaction', amountCredit: "200", amountDebit: "" },
+    { id: 6, narration: 'interbank transaction', amountCredit: "200", amountDebit: "" },
+    { id: 7, narration: 'Nibss transaction', amountCredit: "200", amountDebit: "" },
+    { id: 8, narration: 'pos transaction', amountCredit: "200", amountDebit: "" },
+    { id: 9, narration: 'gtb transaction', amountCredit: "200", amountDebit: "" },
+    { id: 10, narration: 'pos transaction', amountCredit: "200", amountDebit: "" },
+    { id: 11, narration: 'gtb transaction', amountCredit: "", amountDebit: "100" },
+    { id: 12, narration: 'Nibss transaction', amountCredit: "", amountDebit: "100" },
   ]
   ngOnInit(): void {
     this.selectedOption = false;
@@ -44,15 +47,34 @@ export class AppComponent implements OnInit {
   filterFunction(value: any) {
     this.selectedOption = true;
     this.selectedValue = value.name;
-    if (value.name === "all") {
-      this.newArray = this.transactionHistory
-    } else {
-      this.newArray = this.transactionHistory.filter(x => x.narration.split(" ")[0] === value.name);
 
-      console.log(this.newArray);
+    switch (value.name) {
+      case 'all':
+        this.newArray = this.transactionHistory;
+        break;
+      case 'pos':
+      case 'gtb':
+      case 'interbank':
+      case 'Nibss':
+        this.newArray = this.transactionHistory.filter(element => element.narration.split(" ")[0] === value.name);
+        // expected output: "Mangoes and papayas are $2.79 a pound."
+        break;
+      case 'inflow':
+
+        this.newArray = this.transactionHistory.filter(element => element.amountDebit === '');
+        console.log(this.newArray)
+          break;
+      case 'outflow':
+        console.log('outflow');
+        this.newArray = this.transactionHistory.filter(element => element.amountCredit === '');
+        console.log(this.newArray)
+        break
+      default:
+        console.log('ddd');
     }
+   
 
- 
+
   }
 
 }
